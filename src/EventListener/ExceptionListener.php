989ec1c451cdaf23f4c3e51e2ac7463ecc253af8
Sergey Revenko <sergey.revenko@orbitsoft.com>
2019-01-22 11:36:11 +0300

    Adds listener for JSON API exceptions

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * This file is part of the jsonapi-bundle.
+ *
+ * (c) OrbitSoft LLC <support@orbitsoft.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Reva2\JsonApiBundle\EventListener;
+
+use Neomerx\JsonApi\Contracts\Encoder\EncoderInterface;
+use Neomerx\JsonApi\Exceptions\JsonApiException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+/**
+ * JSON API exception handler
+ *
+ * @author Sergey Revenko <sergey.revenko@orbitsoft.com>
+ * @package Reva2\JsonApiBundle\EventListener
+ */
+class ExceptionListener
+{
+    /**
+     * @var EncoderInterface
+     */
+    protected $encoder;
+
+    /**
+     * Constructor
+     *
+     * @param EncoderInterface $encoder
+     */
+    public function __construct(EncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    /**
+     * Converts JSON API exception to response in JSON API format
+     *
+     * @param GetResponseForExceptionEvent $event
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+
+        if ($exception instanceof JsonApiException) {
+            $response = new Response(
+                $this->encoder->encodeErrors($exception->getErrors()),
+                $exception->getHttpCode(),
+                ['Content-Type' => 'application/vnd.api+json']
+            );
+
+            $event->allowCustomResponseCode();
+            $event->setResponse($response);
+        }
+    }
+}
